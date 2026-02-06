b083e225d57c41bb88b5716486f12d13992a4540
Ryan Robson <ringo380@gmail.com>
2026-02-06 02:09:41 -0600

    Add inno formula for innodb-utils v1.2.1

---
+++ Formula/inno.rb
@@ -0,0 +1,33 @@
+class Inno < Formula
+  desc "InnoDB file analysis toolkit for parsing, validating, and inspecting .ibd files"
+  homepage "https://github.com/ringo380/idb-utils"
+  license "MIT"
+  version "1.2.1"
+
+  if OS.mac?
+    if Hardware::CPU.arm?
+      url "https://github.com/ringo380/idb-utils/releases/download/v#{version}/inno-v#{version}-aarch64-apple-darwin.tar.gz"
+      sha256 "93645c553ad98dc8ba450b95a4153f460e92b65b99f68baed011acc34d45cb39"
+    else
+      url "https://github.com/ringo380/idb-utils/releases/download/v#{version}/inno-v#{version}-x86_64-apple-darwin.tar.gz"
+      sha256 "ba2875390903af3c7b9dcc786275fd857e107a314abcba49f8aad337e96020d6"
+    end
+  elsif OS.linux?
+    if Hardware::CPU.arm?
+      url "https://github.com/ringo380/idb-utils/releases/download/v#{version}/inno-v#{version}-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "4354e3e17459963ad37d050f723c3d4eb611be0106657259aaee1d326b0b8618"
+    else
+      url "https://github.com/ringo380/idb-utils/releases/download/v#{version}/inno-v#{version}-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "481846f4d93af1b6434beb8e81c3de4746719356ee43db0335d7295ca4b03681"
+    end
+  end
+
+  def install
+    bin.install "inno"
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/inno --version")
+    assert_match "Usage:", shell_output("#{bin}/inno --help")
+  end
+end
