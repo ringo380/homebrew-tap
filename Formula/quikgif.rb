3a284935d5ebd97793f94ccc31908b48ee8ba0bc
Ryan Robson <ringo380@gmail.com>
2026-03-18 13:52:39 -0500

    Add quikgif formula v1.3.1

---
+++ Formula/quikgif.rb
@@ -0,0 +1,34 @@
+class Quikgif < Formula
+  desc "Record polished GIF demos from the command line"
+  homepage "https://quikgif.com"
+  version "1.3.1"
+  license "MIT"
+
+  depends_on :macos
+  depends_on macos: :sonoma
+
+  url "https://github.com/ringo380/quikgif/releases/download/v#{version}/quikgif"
+  sha256 "e33f32cfbc4e174af42079bcaec57f8b45da0cc864437875dc5639faff0830c8"
+
+  def install
+    bin.install "quikgif"
+  end
+
+  def caveats
+    <<~EOS
+      QuikGIF CLI has been installed!
+
+      Get started:
+        quikgif --help
+        quikgif list-windows
+        quikgif record --duration 10 -o demo.gif
+
+      The QuikGIF macOS app is available separately:
+        https://quikgif.com
+    EOS
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/quikgif --version")
+  end
+end
